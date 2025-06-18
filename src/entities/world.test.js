48b7d80f2446612b9db58e8bafca7f5d0d39f8ab
francesco.barbera <barberaf84@gmail.com>
2025-06-18 06:04:19 +0200

    add a basic test

---
+++ src/entities/world.test.js
@@ -0,0 +1,9 @@
+// test/world.test.ts
+import { test } from "uvu";
+import * as assert from "uvu/assert";
+
+test("initialise calls initialise on all NPCs", async () => {
+  assert.is(true, true);
+});
+
+test.run();
